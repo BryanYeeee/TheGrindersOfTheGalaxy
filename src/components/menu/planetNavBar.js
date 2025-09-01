f6cda32a0ec8535eeffcbf4c47c49182fb3afa9e
BryanYeeee <haloday873@gmail.com>
2025-09-01 01:25:31 -0400

    temp navbar css

--- src/components/menu/planetNavBar.js
+++ src/components/menu/planetNavBar.js
@@ -1,24 +1,56 @@
 'use client'
 import { useGameContext } from '@/context/gameContext'
-import * as Mech from '@/lib/mech/mechExport'
+import { Planet } from '@/lib/mech/mechExport'
+import { AnimatePresence, easeIn, motion } from 'motion/react'
 
 const PlanetNavBar = () => {
-    const { curPlanets, curPlanetKey } = useGameContext()
+  const { curPlanets, curPlanetKey } = useGameContext()
 
-    return (
-        <div className="flex h-12 gap-x-4 p-2">
-            {Array.from(curPlanets).map((planetName, i) => (
-                <button
-                    key={`${planetName}-${i}`}
-                    onClick={() => Mech.Planet.setCurPlanet(planetName)}
-                    className={`p-2 rounded 
-                        ${planetName === curPlanetKey?'bg-slate-600':'bg-gray-700 hover:bg-slate-600'}`}
-                >
-                    {planetName}
-                </button>
-            ))}
-        </div>
-    )
+  const animateVariantConfig = {
+    button: {
+      //   hover: {
+      //     width: '6rem',
+      //     marginInline: '1rem'
+      //   }
+    },
+    spinBorder: {
+      initial: { rotate: 0 },
+      active: { width: '100%' },
+      hover: {
+        rotate: 360,
+        transition: {
+          repeat: Infinity,
+          repeatType: 'loop',
+          ease: 'linear',
+          duration: 3
+        }
+      }
+    }
+  }
+  return (
+    <div className='grid grid-cols-[repeat(auto-fill,minmax(7rem,1fr))] gap-4 px-8 py-4 '>
+      <AnimatePresence>
+        {Array.from(curPlanets).map((planetName, i) => (
+          <motion.button
+            key={`${planetName}-${i}`}
+            onClick={() => Planet.setCurPlanet(planetName)}
+            initial='initial'
+            animate={curPlanetKey === planetName ? 'active' : 'initial'}
+            whileHover='hover'
+            variants={animateVariantConfig.button}
+            className={`relative overflow-hidden w-32 p-[2px] bg-foreground1 grid`}
+          >
+            <motion.div className='z-1 bg-foreground1'>{planetName}</motion.div>
+
+            <motion.div
+              variants={animateVariantConfig.spinBorder}
+              className='absolute h-[600%] bg-accent w-4 left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2'
+            />
+          </motion.button>
+        ))}
+      </AnimatePresence>
+    </div>
+  )
 }
 
 export default PlanetNavBar
